210679c11cdf1838209ef98bacfbcfd7fb4ee5df
isabelamacedo <isabelacb.macedo@gmail.com>
2023-03-06 09:49:18 -0300

    feat: ssr session

---
+++ frontend/src/services/auth/session.js
@@ -0,0 +1,26 @@
+// Decorator Pattern
+
+import { authService } from "./authService";
+
+export function withSession(funcao) {
+  return async (ctx) => {
+    try {
+      const session = await authService.getSession(ctx);
+      const modifiedCtx = {
+        ...ctx,
+        req: {
+          ...ctx.req,
+          session,
+        },
+      };
+      return funcao(modifiedCtx);
+    } catch (err) {
+      return {
+        redirect: {
+          permanent: false,
+          destination: "/?error=unauthorized",
+        },
+      };
+    }
+  };
+}
