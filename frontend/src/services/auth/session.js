5e1e441c26815dd83a5e04a986dcf3832071479a
isabelamacedo <isabelacb.macedo@gmail.com>
2023-03-06 10:51:25 -0300

    feat: client side session

--- frontend/src/services/auth/session.js
+++ frontend/src/services/auth/session.js
@@ -1,7 +1,8 @@
-// Decorator Pattern
-
+import React from "react";
+import { useRouter } from "next/router";
 import { authService } from "./authService";
 
+// Decorator Pattern
 export function withSession(funcao) {
   return async (ctx) => {
     try {
@@ -22,5 +23,49 @@ export function withSession(funcao) {
         },
       };
     }
+  };
+}
+
+export function useSession() {
+  const [session, setSesstion] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState(null);
+
+  React.useEffect(() => {
+    authService
+      .getSession()
+      .then((userSession) => {
+        setSesstion(userSession);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return {
+    data: { session },
+    error,
+    loading,
+  };
+}
+
+export function withSessionHOC(Component) {
+  return function Wrapper(props) {
+    const router = useRouter();
+    const session = useSession();
+
+    if (!session.loading && session.error) {
+      router.push("/?error=401");
+    }
+
+    const modifiedProps = {
+      ...props,
+      session: session.data.session,
+    };
+
+    return <Component {...modifiedProps} />;
   };
 }
